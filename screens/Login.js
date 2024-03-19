7c806ef4f61ccfc6e7dfd5b807d2e66a7632a9d4
Nity-a-priya <nityapriya.katta@thoughtworks.com>
2024-03-19 12:28:57 +0530

    [Umair|Aravind|Supraja|Nitya] initial login page

---
+++ screens/Login.js
@@ -0,0 +1,29 @@
+import { Button, TextInput, View, StyleSheet } from "react-native";
+
+function Login() {
+  return (
+    <View style={styles.rootContainer}>
+      <TextInput placeholder="Enter your name!" style={styles.textInput} />
+
+      <Button title="Submit" />
+    </View>
+  );
+}
+
+export default Login;
+const styles = StyleSheet.create({
+  rootContainer: {
+    margin: 50,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "grey",
+    padding: 16,
+    borderRadius: 5,
+    marginHorizontal: 16,
+    width: "80%",
+    marginBottom: 10,
+  },
+});
